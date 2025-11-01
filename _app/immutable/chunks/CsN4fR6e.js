fbd53b3952b8b0810f3230923e96b2f1af700b24
jairomelo <jairomelo@users.noreply.github.com>
2025-11-01 21:42:28 +0000

    deploy: a40852fe529872e9e5371832da41743fe9fab5fd

---
+++ _app/immutable/chunks/CsN4fR6e.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1a82hec?.base??"/graphical-archive",a=globalThis.__sveltekit_1a82hec?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.BLniMRGU.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Cu3n296i.js";export{o as load_css,r as start};

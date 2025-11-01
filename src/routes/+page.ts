12660909be6e49c179514b5ea2a2b98f7fda7aed
jairomelo <jairoantoniomelo@gmail.com>
2025-11-01 14:30:50 -0700

    fix paths to fit with Github pages

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,12 @@
+import { base } from '$app/paths';
+
 export const ssr = false;
 export const prerender = true;
 
 export async function load({ fetch }) {
   const [metaResp, neighResp] = await Promise.all([
-    fetch('/data/europeana_metadata.json'),
-    fetch('/data/europeana_neighbors.json')
+    fetch(`${base}/data/europeana_metadata.json`),
+    fetch(`${base}/data/europeana_neighbors.json`)
   ]);
   return {
     metadata: await metaResp.json(),

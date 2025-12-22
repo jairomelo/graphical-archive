bfeba6fc54c7f3f193c32568ddafa93de7cce863
jairomelo <jairoantoniomelo@gmail.com>
2025-12-21 16:38:05 -0800

    fix: update fetch paths to use asset helper for metadata and neighbors

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,12 @@
+import { asset } from '$app/paths';
+
 export const ssr = false;
 export const prerender = true;
 
 export async function load({ fetch }) {
   const [metaResp, neighResp] = await Promise.all([
-    fetch('/data/europeana_metadata.json'),
-    fetch('/data/europeana_neighbors.json')
+    fetch(asset('/data/europeana_metadata.json')),
+    fetch(asset('/data/europeana_neighbors.json'))
   ]);
   return {
     metadata: await metaResp.json(),

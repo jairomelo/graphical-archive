238a9ade841d7623aed7fc3d52feedbb0ca3c116
jairomelo <jairomelo@users.noreply.github.com>
2025-12-30 18:51:45 +0000

    deploy: 5129fdbc437f34978eb2fca8a3c206a486050fcd

--- _app/immutable/chunks/9J9VfSXx.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1am70w?.base??"/graphical-archive",a=globalThis.__sveltekit_1am70w?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/CuwJ6ud-.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_10bahyv?.base??"/graphical-archive",a=globalThis.__sveltekit_10bahyv?.assets??s??"";export{a,s as b};
